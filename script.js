aa38ffff6ee6d83150bbd57e88113fd0ecc8f396
lrueegsegger1 <lukas.rueegsegger@gmx.ch>
2024-09-17 20:21:35 +0200

    Styled background and added mobile support

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-//Background color
-document.addEventListener('mousemove', updateBackground);
-function updateBackground(e) {
-    let x = e.clientX / window.innerWidth;
-    let angle = Math.round(x * 360);
-    document.body.style.background = `linear-gradient(${angle}deg, #564875, #222)`;
-}
-
 //Loading boxes in
 document.addEventListener('DOMContentLoaded', function() {
     const textBoxes = document.querySelectorAll('.text-box');
